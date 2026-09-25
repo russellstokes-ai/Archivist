6d416f186ab52375ca9914a9b42132f53bbf64db
Bigmac <russell.stokes@gmail.com>
2026-09-25 12:17:27 +0100

    Return editable author and series metadata in library API

--- home-assistant/archivist/app/main.go
+++ home-assistant/archivist/app/main.go
@@ -40,6 +40,8 @@ type source struct {
 type book struct {
 	ID        int64  `json:"id"`
 	Title     string `json:"title"`
+	Author    string `json:"author"`
+	Series    string `json:"series"`
 	Format    string `json:"format"`
 	Space     string `json:"space"`
 	Available bool   `json:"available"`
@@ -348,7 +350,7 @@ func (a *app) routes() http.Handler {
 	mux.HandleFunc("GET /api/books", func(w http.ResponseWriter, r *http.Request) {
 		q := "%" + r.URL.Query().Get("q") + "%"
 		space := r.URL.Query().Get("space")
-		rows, e := a.db.Query(`SELECT a.id,a.title,a.format,s.space,a.available FROM assets a JOIN sources s ON s.id=a.source_id WHERE a.title LIKE ? AND (?='' OR s.space=?) AND (? OR s.space IN (SELECT space FROM grants WHERE profile_id=?)) ORDER BY a.title,a.id LIMIT 500`, q, space, space, who(r).Owner, who(r).ID)
+		rows, e := a.db.Query(`SELECT a.id,a.title,a.author,a.series,a.format,s.space,a.available FROM assets a JOIN sources s ON s.id=a.source_id WHERE a.title LIKE ? AND (?='' OR s.space=?) AND (? OR s.space IN (SELECT space FROM grants WHERE profile_id=?)) ORDER BY a.title,a.id LIMIT 500`, q, space, space, who(r).Owner, who(r).ID)
 		if e != nil {
 			fail(w, 500, e)
 			return
@@ -357,7 +359,7 @@ func (a *app) routes() http.Handler {
 		out := []book{}
 		for rows.Next() {
 			var b book
-			if e = rows.Scan(&b.ID, &b.Title, &b.Format, &b.Space, &b.Available); e != nil {
+			if e = rows.Scan(&b.ID, &b.Title, &b.Author, &b.Series, &b.Format, &b.Space, &b.Available); e != nil {
 				fail(w, 500, e)
 				return
 			}
