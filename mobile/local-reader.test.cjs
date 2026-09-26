cb08acca5482c163a230002ab5737eaf3bf35977
Bigmac <russell.stokes@gmail.com>
2026-09-26 23:13:42 +0100

    Fix reader sanitization assertion for trusted interaction script

--- mobile/local-reader.test.cjs
+++ mobile/local-reader.test.cjs
@@ -26,7 +26,7 @@ const {buildLocalReaderDocument} = require('./localReader.ts');
   files.set('book.epub', await zip.generateAsync({type: 'base64'}));
   const epub = await buildLocalReaderDocument('book.epub', 'EPUB', 'Dune');
   assert(epub.html.includes('Arrakis'));
-  assert(!epub.html.includes('<script>'));
+  assert(!epub.html.includes('bad()'));
   assert(!epub.html.includes('onclick'));
   assert(epub.html.includes('column-width'));
   assert(epub.html.includes('text-focused'));
